Fix wrong calculation of character error rate
The problem was that when calculating CER, special tokens (<PAD>, <SOS>,
<EOS>) were included in the target and predicted string of tokens.

src/lit_models.py
@@ -1,25 +1,28 @@
 from typing import Callable, Optional, Dict, Union, Any
 
 from models import FullPageHTREncoder, FullPageHTRDecoder
-from metrics import CharacterErrorRate, WordErrorRate
+
+# from metrics import CharacterErrorRate, WordErrorRate
+from metrics import CharacterErrorRate
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import pytorch_lightning as pl
 from torch import Tensor
+from sklearn.preprocessing import LabelEncoder
 
 
 class LitFullPageHTREncoderDecoder(pl.LightningModule):
     encoder: FullPageHTREncoder
     decoder: FullPageHTRDecoder
     cer_metric: CharacterErrorRate
-    wer_metric: WordErrorRate
+    # wer_metric: WordErrorRate
     loss_fn: Callable
 
     def __init__(
         self,
+        label_encoder: LabelEncoder,
         encoder_name: str,
         vocab_len: int,
         d_model: int,
@@ -73,8 +76,8 @@ class LitFullPageHTREncoderDecoder(pl.LightningModule):
         )
 
         # Initialize metrics and loss function.
-        self.cer_metric = CharacterErrorRate()
-        self.wer_metric = WordErrorRate()
+        self.cer_metric = CharacterErrorRate(label_encoder)
+        # self.wer_metric = WordErrorRate()
         self.loss_fn = nn.CrossEntropyLoss(ignore_index=self.decoder.pad_tkn_idx)
 
     def forward(self, imgs: Tensor):
@@ -93,23 +96,23 @@ class LitFullPageHTREncoderDecoder(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         imgs, targets = batch
-        B, T = targets.shape
-
         logits, _ = self(imgs)
         _, preds = logits.max(-1)
 
-        # Log metrics and loss.
+        # Calculate metrics and loss.
         cer = self.cer_metric(preds, targets)
-        wer = self.wer_metric(preds, targets)
+        # wer = self.wer_metric(preds, targets)
         loss = self.loss_fn(
-            logits[:, :T, :].transpose(1, 2), targets[:, : logits.size(1)]
+            logits[:, : targets.size(1), :].transpose(1, 2),
+            targets[:, : logits.size(1)],
         )
+        # Log metrics and loss.
         self.log("char_error_rate", cer, prog_bar=True)
-        self.log("word_error_rate", wer)
+        # self.log("word_error_rate", wer)
         self.log("val_loss", loss, sync_dist=True, prog_bar=True)
         self.log(
             "hp_metric", cer
-        )  # this will show up in the Tensorboard hparams tab, for comparing different models
+        )  # this will show up in the Tensorboard hparams tab, used for comparing different models
 
         return loss
 